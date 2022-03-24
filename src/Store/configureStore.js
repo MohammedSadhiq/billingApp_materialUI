467637e01633a0dbeef0bc5b7fd862c612cd0595
MohammedSadhiq <53676096+MohammedSadhiq@users.noreply.github.com>
2022-03-24 09:05:01 +0530

    razor pay

---
+++ src/Store/configureStore.js
@@ -0,0 +1,23 @@
+import { createStore } from "redux";
+import { combineReducers } from "redux";
+import { applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import billsReducer from "../Reducers/billsReducer";
+import customerReducer from "../Reducers/customerReducer";
+import productReducer from "../Reducers/productReducer";
+import userReducer from "../Reducers/userReducer";
+import loginReducer from "../Reducers/loginReducer";
+
+
+const configureStore = ()=>{
+    const store = createStore(combineReducers({
+        bills: billsReducer,
+        customers: customerReducer,
+        user: userReducer,
+        login : loginReducer,
+        product:productReducer
+    }),applyMiddleware(thunk))
+    return store
+}
+
+export default configureStore
